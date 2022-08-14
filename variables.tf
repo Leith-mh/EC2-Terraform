7f243992f87e59aa23179b082d9eeb1644e6c93a
leith <leith.mhf@gmail.com>
2022-08-14 18:41:40 +0100

    vars updat

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
-variable myip {}
+variable my_ip {}
 variable public_key_location {}
 variable instance_type {}
+variable image_name {}
