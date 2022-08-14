bb3e73e4b9a00b37f3570260b1b0a39db5ae5331
leith <leith.mhf@gmail.com>
2022-08-14 17:50:17 +0100

    call child module in main

--- main.tf
+++ main.tf
@@ -7,13 +7,7 @@ terraform {
   }
 }
 
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable myip {}
-variable public_key_location {}
-variable instance_type {}
+
 
 provider "aws" {
   region = "eu-west-3"
@@ -26,53 +20,20 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id 
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name: "${var.env_prefix}-subnet-1"
-    }
-}
 
-/*resource "aws_route_table" "myapp-route-table" {
+module "mypass-subnet" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
     vpc_id = aws_vpc.myapp-vpc.id
-    
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-        Name: "${var.env_prefix}-rtb"
-    }
-}*/
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table
 
-
-resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-        Name: "${var.env_prefix}-igw"
-    }
-}
-
-/*resource "aws_route_table_association" "a-rtb-subnet" {
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    route_table_id = aws_route_table.myapp-route-table.id
-}*/
-
-resource "aws_default_route_table" "main-rtb" {
-    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-    
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-        tags = {
-            Name: "${var.env_prefix}-main-rtb"
-    }
 
 
 }
+
+
 
 resource "aws_default_security_group" "default-sg" {
     vpc_id = aws_vpc.myapp-vpc.id
@@ -105,9 +66,7 @@ resource "aws_default_security_group" "default-sg" {
 }
 
 
-output "server-ip" {
-    value = aws_instance.myapp-server.public_ip
-}
+
 
 
 data "aws_ami" "latest-amazon-linux-image" {
@@ -123,13 +82,7 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
-output "aws_ami_id" {
-    value = data.aws_ami.latest-amazon-linux-image.id
-}
 
-output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
-}
 
 resource "aws_key_pair" "ssh-key" {
     key_name = "server-key"
@@ -140,7 +93,7 @@ resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
 
-    subnet_id = aws_subnet.myapp-subnet-1.id
+    subnet_id = module.myapp-subnet.subnet.id
     vpc_security_group_ids = [aws_default_security_group.default-sg.id]
     availability_zone = var.avail_zone
 
